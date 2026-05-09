c32c69198d0352ed23a31b700f0ddc9487cd7030
BryanSouza <bryancsouza@gmail.com>
2026-05-09 17:31:11 +0200

    implementing cards style and widget

--- content/board.md
+++ content/board.md
@@ -1,30 +1,83 @@
-# Challenge board
-::::{grid} 1 1 2 3
+---
+site:
+  hide_outline: true
+---
 
-:::{card} 
-:header: **Challenge 1**
-Exercise generator (easy) 
+# Challenge board
+::::{grid} 1 
+:class: col-page-right
+
+:::{grid-item-card} 
+:header: **Exercise generator**
+<!-- <span class="text-gradient" style="font-weight:bold;"><em>Write a program that  ..</em></span> -->
 ```{anywidget} random-question.mjs
+:css: ./random-question.css
 {
   "count": 0,
   "question": ["Write a program that converts from "],
   "field1": ["int", "float", "str", "bool"],
   "field2": ["dict", "tuple", "list"]
-
 }
 ```
 :::
-:::{card} 
-:header: **Challenge 2**
+::::
+
+::::{grid} 1 1 2 3
+:class: col-page-right
+
+:::{grid-item-card} 
+:header: <span style = "color : #6c8308; font-weight:bold;">A Study with Strings</span>
 :link: ./a-study-with-strings
-A Study with Strings
-
+<span class="tag str">str</span>
+<span class="tag loops">loops</span>
+<span class="tag ifs">if statements</span>
 :::
 
-:::{card} 
-:header: **Challenge 3**
-:link: ./neuro-signal-quest1
+:::{card}
+:link: ./neuro-signal-quest1 
+:header: <span style = "color : #3803e4; font-weight:bold;">Neuro Signal Quest 1</span>
 
-Neuro Signal Quest 1
+<span class="tag array">arrays</span>
+<span class="tag plots">plots</span>
+<span class="tag neuro">neuro</span>
+:::
+
+
+:::{card}
+:header: **New Card**
+:link: ./a-study-with-strings
+:class-header: 
+
+<span class="tag array">arrays</span>
+<span class="tag plots">plots</span>
+<span class="tag str">str</span>
+<span class="tag loops">loops</span>
+<span class="tag ifs">if statements</span>
+<span class="tag neuro">neuro</span>
+<span class="tag cryptography">cryptography</span>
 
 :::
+<!-- 
+- [ ] str
+- [ ] list
+- [ ] dict
+- [ ] arrays
+- [ ] tuple
+ -->
+
+<!-- Here's some <span class="text-gradient" style="font-weight:bold;">span <em>styled</em></span> content
+
+```{note}
+:class: text-gradient
+I'm _very stylish_.
+```
+
+:::{div}
+:class: tag str
+Test
+::: -->
+
+
+<!-- <div class="text-gradient" style="font-weight: bold;">Here's my <em>div</em></div>
+
+Here's some <span class="text-gradient" style="font-weight:bold;">span <em>styled</em></span> content -->
--- content/random-question.mjs
+++ content/random-question.mjs
@@ -1,3 +1,8 @@
+function randomIdx(max){
+  return Math.floor(Math.random() * max)
+}
+
+
 function render({ model, el }) {
 
   // Setup quick-access to state
@@ -6,7 +11,6 @@ function render({ model, el }) {
   const setQuestion = (question) => model.set('question', question);
 
   const getField1 = () => model.get('field1');
-//   const getField2 = () => model.get('field2');
   
   const getCount = () => model.get('count');
   const setCount = (count) => model.set('count', count);
@@ -17,40 +21,75 @@ function render({ model, el }) {
   const getJ = () => model.get('j');
   const setJ = (j) => model.set('j', j);
   
-  
-  let txt = document.createElement('div');
-  txt.textContent = "?";
 
+//   var img = document.createElement('img')
+
+//   img.setAttribute("src", 'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png');
+//   img.setAttribute("width", "304");
+//   img.setAttribute("height", "228");
+//   img.setAttribute("alt", "The Image");
+  
+  
+//   fetch('./PythonScroll.png')
+//     .then(response => response.blob()) 
+//     .then(blob => {
+//         let imgURL = URL.createObjectURL(blob);
+//         img.setAttribute("src", imgURL);
+//     })
+//     .catch(error => console.error("Error fetching image:", error));
+
+    let colors = [
+        "#26ccff",
+        "#a25afd",
+        "#ff5e7e",
+        "#56a139",
+        "#797a22",
+        "#ffa62d",
+        "#c829c8"
+        ]
+        
+
+
+  let txt = document.createElement('div');
+  txt.classList.add('question-text');
   
   // Create button
   let btn = document.createElement('button');
-  btn.classList.add('counter-button');
+  btn.classList.add('question-button');
 
-  setCount(Math.floor(Math.random() * getQuestion().length))
-  setI(Math.floor(Math.random() * getField1().length));
-  setJ(Math.floor(Math.random() * getField1().length));
-  btn.innerHTML = `Generate!`
-//   txt.textContent = `${getQuestion()[0]} ${getField1()[getI()]} to ${getField1()[getJ()]}`;
+  setCount(randomIdx(getQuestion().length))
+  setI(randomIdx(getField1().length));
+  setJ(randomIdx(getField1().length));
+  btn.innerHTML = `New question`
 
 
   // Handle button click
   btn.addEventListener('click', () => {
-    setCount(Math.floor(Math.random() * getQuestion().length))
-    setI(Math.floor(Math.random() * getField1().length));
-    setJ(Math.floor(Math.random() * getField1().length));
+    
+    setCount(randomIdx(getQuestion().length))
+    setI(randomIdx(getField1().length));
+    setJ(randomIdx(getField1().length));
     model.save_changes();
   });
 
   // Update text when count changes
   model.on('change:count', () => {
-  txt.textContent = `${getQuestion()[getCount()]} ${getField1()[getI()]} to ${getField1()[getJ()]}`;
-
+  txt.textContent = `${getQuestion()[getCount()]} ${getField1()[randomIdx(getField1().length)]} to ${getField1()[getJ()]}`;
+  // txt.setHTMLUnsafe("`${getQuestion()[getCount()]} <span style="color:red">${getField1()[randomIdx(getField1().length)]}</span> to ${getField1()[getJ()]}`;")
+  Object.assign(txt.style, {
+    color: colors[randomIdx(colors.length)]
+    });
+  
   });
   
+//   el.appendChild(img);
   el.appendChild(txt);
   el.appendChild(btn);
 
   // Destructor to clean-up when MyST is finished with us!
-  return () => btn.remove();
+  return () => {
+    btn.remove();
+    txt.remove();
+  }
 }
 export default { render };
